bd4f4f148ce2eb1ab980b3652ce32c640a98c839
chenpoyang <young.cpy@gmail.com>
2012-05-17 22:20:27 +0800

    last editting:get_filename

--- wwidget.cpp
+++ wwidget.cpp
@@ -1,17 +1,73 @@
+#include <QFileDialog>
+#include <QMessageBox>
 #include "wwidget.h"
 
 WWidget::WWidget(QWidget *parent) :
 	QWidget(parent)
 {
     init_widget();
-}
-
-WWidget::~WWidget()
-{
+    init_connection();
 }
 
 /**
- * @brief	初始化控件
+ * @brief	初始化WWidget类的信号与槽
+ */
+void WWidget::init_connection()
+{
+    /* 第一个按钮的事件响应 */
+    connect(&fir_file_btn, SIGNAL(clicked()), this, SLOT(fir_filename_from_fdlg()));
+    /* 第二个按钮的事件响应 */
+    connect(&sec_file_btn, SIGNAL(clicked()), this, SLOT(sec_filename_from_fdlg()));
+
+    /* 执行移除重复行操作 */
+    connect(&process_btn, SIGNAL(clicked()), this, SLOT(exec_remove_lines()));
+}
+
+/* 执行两件文的操作 */
+void WWidget::exec_remove_lines()
+{
+    fir_file_str = fir_file_le.text();
+    sec_file_str = sec_file_le.text();
+    judge_file(fir_file_str);
+    judge_file(sec_file_str);
+}
+
+/* 判断文件是否存在 */
+bool WWidget::judge_file(const QString filename)
+{
+    QFile file(filename);
+
+    if (!file.open(QFile::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this,
+            tr("Warnning!"),
+            tr("Cannot open the file, make sure the file is exist!"),
+            QMessageBox::Cancel, QMessageBox::Ok);
+
+        return false;
+    }
+    file.close();
+
+    return true;
+}
+/* 从文件对话框中选择一个文件 */
+void WWidget::fir_filename_from_fdlg()
+{
+    fir_file_str = QFileDialog::getOpenFileName(this,
+                    tr("Open Text"), tr("./"), tr("Text Files (*.txt)"));
+    fir_file_le.setText(fir_file_str);    /* 设置单行文本框值 */
+}
+
+/* 从文件对话框中选择第二个文件 */
+void WWidget::sec_filename_from_fdlg()
+{
+    sec_file_str = QFileDialog::getOpenFileName(this,
+                    tr("Open Text"), tr("./"), tr("Text Files (*.txt)"));
+    sec_file_le.setText(sec_file_str);  /* 设置单行文本框值 */
+}
+
+/**
+ * @brief    初始化控件
  */
 void WWidget::init_widget()
 {
@@ -23,6 +79,7 @@ void WWidget::init_widget()
     fir_file_btn.setText(QObject::tr("choose file1"));
     sec_file_btn.setText(QObject::tr("choose file2"));
     process_btn.setText(QObject::tr("process"));
+    process_btn.setShortcut(QObject::tr("Enter"));
 
     /* 布局管理器管理控件 */
     fir_file_le.resize(150, 20);
@@ -37,9 +94,19 @@ void WWidget::init_widget()
     setLayout(&layout);
 }
 
-/**
- * @brief	初始化WWidget类的信号与槽
- */
-void WWidget::init_connection()
+/* 获取文件名 */
+QString WWidget::get_filename(const QString filename)
+{
+    bool exist = false;
+
+    exist = judge_file(filename);
+    if (!exist)
+    {
+        return;
+    }
+/* last editting...!!!!, continue here next time */
+}
+
+WWidget::~WWidget()
 {
 }
--- wwidget.h
+++ wwidget.h
@@ -5,41 +5,48 @@
 #include <QtGui/QPushButton>
 #include <QtGui/QLineEdit>
 #include <QtGui/QGridLayout>
+#include <QtCore/QFile>
 #include <QtCore/QString>
 
 /**
- * @brief	WWidget: wps widget类, 从QT库的QWidget类继承
+ * @brief    WWidget: wps widget类, 从QT库的QWidget类继承
  */
 class WWidget : public QWidget
 {
-	Q_OBJECT
+    Q_OBJECT
 
 public:
-	explicit WWidget(QWidget *parent = 0);
-	virtual ~WWidget();
+    explicit WWidget(QWidget *parent = 0);
+    virtual ~WWidget();
 
 private:
-	void init_widget();	/* 初始化控件 */
-	void init_connection();	/* 初始化信号与槽 */
+    void init_widget();    /* 初始化控件 */
+    void init_connection();    /* 初始化信号与槽 */
+    bool judge_file(const QString filename);  /* 判断文件是否存在 */
+    QString get_filename(const QString filename);    /* 获取文件名 */
 
 signals:
 
 private slots:
+    void fir_filename_from_fdlg();    /* 从文件对话框中选择第一个文件 */
+    void sec_filename_from_fdlg();    /* 从文件对话框中选择第二个文件 */
+    void exec_remove_lines();         /* 执行两件文的操作 */
 
 private:
-	/* 两个选择文件的按钮, 及一个执行命令的按钮 */
-	QPushButton fir_file_btn, sec_file_btn;
-	/* 两个文本输入域, 用于保存输入文件的绝对路径名 */
-	QLineEdit fir_file_le, sec_file_le;
-	/* 两文件的绝对路径名 */
-	QString fir_file_str, sec_file_str;
+    /* 两个选择文件的按钮, 及一个执行命令的按钮 */
+    QPushButton fir_file_btn, sec_file_btn;
+    /* 两个文本输入域, 用于保存输入文件的绝对路径名 */
+    QLineEdit fir_file_le, sec_file_le;
+    /* 两文件的绝对路径名 */
+    QString fir_file_str, sec_file_str;
 
     /* 执行按钮 */
     QPushButton process_btn;
 
     /* 布局管理器 */
-    QHBoxLayout fir_layout, sec_layout;
     QGridLayout layout;
+
+    QFile file_one, file_two;
 };
 
 #endif /* __WWIDGET_H__ */
